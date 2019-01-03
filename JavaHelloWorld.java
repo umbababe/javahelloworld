d6e0eb3b6d7b4e90420e90abd7defbe628827485
umbababe <sysinfo@daum.net>
2019-01-03 11:22:43 +0900

    hello to how do you do

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -3,6 +3,6 @@ public class JavaHelloWorld
   public static void main (String [] args)
   {
     System.out.println("Java Hello World");
-    System.out.println("Hello again");
+    System.out.println("How do you do again");
   }
 }
